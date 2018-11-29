f0b23b8b6a98c4f52a40d6fa770c331cfc3dbd3b
Jamalzade Elvin Rasim  <Jama_yw17@ITSTEP.LOCAL>
2018-11-29 19:26:23 +0400

    Add project files.

---
+++ CourseProject/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CourseProject
+{
+    class User
+    {
+        public User()
+        {
+
+        }
+        public User(string email, string username, string status, string password)
+        {
+            Email = email;
+            Username = username;
+            Status = status;
+            Password = password;
+        }
+
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Status { get; set; }
+        public string Password { get; set; }
+
+    }
+    class Worker : User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Education { get; set; }
+        public string City { get; set; }
+        public decimal MinSalary { get; set;}
+        public string PhoneNumber { get; set;}
+        public List<string> Experience = new List<string>() {
+             "1 ilden asagi",
+             "1 ilden - 3 ile qeder",
+             "3 ilden - 5 ile qeder",
+             "5 ilden daha cox"
+        };
+        public List<string> Category = new List<string>()
+        {
+            "Programmer","Journalist","IT Specialist"
+        };
+        public Worker(string name, string surname, int age, string gender, string education, string city, decimal minSalary, string phoneNumber, User user) : base(user.Email, user.Username, user.Status, user.Password)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gender = gender;
+            Education = education;
+            City = city;
+            MinSalary = minSalary;
+            PhoneNumber = phoneNumber;
+        }
+    }
+
+    class Employee :User { }
+    class Controller { }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
