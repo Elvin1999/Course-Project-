a8c15fefc86d2ed638c7f095b0d1dfe5014b000f
camalzade_elvin <camalzade_elvin@outlook.com>
2018-11-29 20:45:44 +0400

    category

--- CourseProject/Program.cs
+++ CourseProject/Program.cs
@@ -43,17 +43,18 @@ namespace CourseProject
         public string City { get; set; }
         public decimal MinSalary { get; set; }
         public string PhoneNumber { get; set; }
+        public int Category { get; set; }
         public List<string> Experience = new List<string>() {
              "1 ilden asagi",
              "1 ilden - 3 ile qeder",
              "3 ilden - 5 ile qeder",
              "5 ilden daha cox"
         };
-        public List<string> Category = new List<string>()
+        public List<string> Categories = new List<string>()
         {
             "Programmer","Journalist","IT Specialist"
         };
-        public Worker(string name, string surname, int age, string gender, string education, string city, decimal minSalary, string phoneNumber, User user)
+        public Worker(string name, string surname, int age, string gender,int category, string education, string city, decimal minSalary, string phoneNumber, User user)
             : base(user.Email, user.Username, user.Status, user.Password)
         {
             Name = name;
@@ -64,6 +65,7 @@ namespace CourseProject
             City = city;
             MinSalary = minSalary;
             PhoneNumber = phoneNumber;
+            Category = category;
         }
         public void ShowWorker()
         {
@@ -76,6 +78,7 @@ namespace CourseProject
             Console.WriteLine($"City - > {City}");
             Console.WriteLine($"Minimum Salary - > {MinSalary}");
             Console.WriteLine($"Phone number - > {PhoneNumber}");
+            Console.WriteLine($"Category - > {Categories[Category]}");
             ShowUserProperty();
         }
     }
@@ -86,16 +89,16 @@ namespace CourseProject
 
         public void Run()
         {
-            //User user = new User("camalzade_elvin@mail.ru", "Elvin1999", "Worker", "123456798");
-            //Worker worker = new Worker("Elvin", "Camalzade", 19, "Male", "Bachelor", "Baku", 800m, "0515848762", user);
-            //worker.ShowWorker();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("=============================================================================");
-            Console.WriteLine("|                                                                           |");
-            Console.WriteLine("|                         ADVERTISEMENT SITE                                |");
-            Console.WriteLine("|                                                                           |");
-            Console.WriteLine("=============================================================================");
-            Console.WriteLine(); Console.Write("SIGN IN (1) SIGN UP (2)");
+            User user = new User("camalzade_elvin@mail.ru", "Elvin1999", "Worker", "123456798");
+            Worker worker = new Worker("Elvin", "Camalzade", 19, "Male",1, "Bachelor", "Baku", 800m, "0515848762", user);
+            worker.ShowWorker();
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine("\t\t\t=============================================================================");
+            //Console.WriteLine("\t\t\t||                                                                         ||");
+            //Console.WriteLine("\t\t\t||                        ADVERTISEMENT SITE                               ||");
+            //Console.WriteLine("\t\t\t||                                                                         ||");
+            //Console.WriteLine("\t\t\t=============================================================================");
+            Console.WriteLine(); Console.Write("\t\t\t\tSIGN IN (1) SIGN UP (2)");
             int selection = Convert.ToInt32(Console.ReadLine());
         }
     }
